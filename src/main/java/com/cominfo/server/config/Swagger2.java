3c5d7c020898e99df03d4d99cb164637e3e15886
Eturial <1220405381@qq.com>
2021-03-29 21:01:15 +0800

    注册接口

--- src/main/java/com/cominfo/server/config/Swagger2.java
+++ src/main/java/com/cominfo/server/config/Swagger2.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -33,6 +34,8 @@ public class Swagger2 {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("ComInfo")
+                .contact(new Contact("Eturial","localhost:8888/doc.html", "1220405381@qq.com"))
+                .version("1.0")
                 .description("api")
                 .build();
     }
